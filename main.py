609af1c34956b4582921b874787bc8445f2a17bc
hatnis1 <127141603+hatns@users.noreply.github.com>
2023-03-16 21:18:28 +0100

    MAIN BUILD

---
+++ main.py
@@ -0,0 +1,29 @@
+from PIL import Image
+from tkinter import Tk, filedialog
+root = Tk()
+root.withdraw()
+file_path = filedialog.askopenfilename()
+img = Image.open(file_path)
+width, height = img.size
+pixels = [[0 for x in range(width)] for y in range(height)]
+for y in range(height):
+    for x in range(width):
+        pixels[y][x] = img.getpixel((x, y))
+import random
+import string
+file_name = "".join(random.sample(string.ascii_letters+string.digits, 15))
+command = f'''import pygame
+def get_surface():
+    surface = pygame.Surface({img.size}, pygame.SRCALPHA, 32)
+'''
+for y, row in enumerate(pixels):
+    for x, col in enumerate(row):
+        command += f'''
+    r, g, b, a = {col}
+    square_surface = pygame.Surface((1,1), pygame.SRCALPHA)
+    square_surface.fill((r,g,b))
+    square_surface.set_alpha(a)
+    surface.blit(square_surface, ({x},{y}))
+    '''
+command += "return surface"
+open("output_"+file_name+".py", "w").write(command)
